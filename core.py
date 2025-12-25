ecd61d6f956673b93244150509ffd8edded4f8d8
danialthecoder <danialtakallou@gmail.com>
2025-12-25 22:08:12 +0330

    Update core.py

--- core.py
+++ core.py
@@ -1,7 +1,8 @@
 from database import get_session
 from utils import hash_password, check_password
 from models import Customer, Account
-
+import random
+from datetime import datetime
 
 
 class AdminPanel:
@@ -11,14 +12,17 @@ class AdminPanel:
 
 
     def create_customer(self,name,email,age,phone,address):
-        #row tooye database besazam
+        customer= Customer(
+        name=Mohammed,
+        email=Mohammed@gmail.com,
+        age=37,
+        phone=09196274398
+        address=Iran, Tehran, Tajrish, 1st Street, 61298-92641, Floor 1
         
-        #^^^
-        #kare shomast *** age, phone, address tooye models.py inja ham bezarid *******
+        
         customer= Customer(name=name,email=email)
 
-        #ta alan classesho sakhti tooye python
-        #zakhire bshe?? tooey db
+        # Save to DB
         self.session.add(customer)
         self.session.commit()
         print(f'customer {name} created successfully')
@@ -26,23 +30,23 @@ class AdminPanel:
 
     def create_account(self,customer_id,account_type,balance, pin):
         customer=self.session.get(Customer,customer_id)
-        #row ro bekesham biron
-
-        if not customer:
-            #^^^
+         if not customer:
             raise Exception(f'Customer with id {customer_id} not found')
 
-        #^^^
-        #card number shoam ezafe kon?????
-        #numpy ---> gpt beporsid ???
-        #card_number=328273817
-
+        card_number = ''.join(str(random.randint(0, 9)) for _ in range(16))
         hashed_pin=hash_password(pin)
 
         account= Account(balance=balance,type=account_type,pin=hashed_pin)
+        account= Account(
+            balance=balance
+            pin: hashed_pin,
+            card_number=card_number,
+            customer_id=customer.id
+        
+        )
         self.session.add(account)
         self.session.commit()
-
+        print(f'Account for customer {customer.name} created successfully')
         return account
 
 
@@ -51,49 +55,84 @@ class AdminPanel:
     def show_balance(self,account_id):
         account=self.session.get(Account,account_id)
         if not account:
-            #^^
             raise Exception(f'Account with id {account_id} not found')
 
         balance= account.balance
 
-        #^^ print
-        return balance
+        print()
+        return account.balance
 
     
     def deposit(self,account_id,amount):
         account=self.session.get(Account,account_id)
         if not account:
-            #^^
             raise Exception(f'Account with id {account_id} not found')
 
-        account.balance=account.balance + amount
+        account.balance+= amount
+        txn = Transaction(
+            account_id=account.id,
+            amount=amount,
+            timestamp= datetime.utcnow
+        )
+        self.session.add(txn)
         self.session.commit()
         return account
+                    
 
     def withdraw(self,account_id,amount):
-        #^^^^^^^^
-        '''
-        hatman check kone amount <balance
-        '''
-        pass
+        account= self.session.get(Account, account_id)
+        if not account:
+            raise Exception(f'Account with id {account_id} not found')
+        if amount > account.balance:
+            raise 
+        pass Exception("Insufficient funds")
+
+
+        account.balance -= amount
+        txn = Transaction(
+            account-id=account.id,
+        amount=amount
+        type="withdraw',
+        timestamp=datetime.utcnow()
+    )
+    self.session.add(txn)
+    self.session.commit()
+    return account 
 
 
     def transfer(self,from_account_id,to_account_id,amount):
-        '''
-        account from --> pull balance+ --> farde dg
-        '''
+        from_account = self.session.get(Account, from_account_id)
+        to_account = self.session.get(Account, to_account_id)
+        if not from_account or not to_account:
+            raise Exception("One or both accounts not found")
+        if amount > from_account.balance:
+            raise Exception("Insufficient funds in source account")
 
+        from_account.balance -= amount
+        to_account.balance += amount
+
+        out_txn= Transaction(
+            account_id=from_account.id,
+            amount=amount
+            type="transfer-out"
+            timestamp=datetime.utcnow()
+        )
+        in-txn= Transaction(
+            account-id=to_account.id,
+            amount=amount,
+            type="transfer-in",
+            timestamp=datetime.utcnow()
+        )
+        
+        self.session.add_all([out_txn, in_txn])
+        self.session.commit()
+        return from_account, to_account
+        
         pass
 
     def show_transaction(self,account_id):
-        '''
-        tarakonesh haro neshoon bede
-
-        ** Models.py --> Class transaction --> id ,amount, type=deposit ya witdhraw, tim,
-        dposit() withdraw() --> ye radis tooye clas transaction
-        
-
-        '''
+        transactions = self.session.query(Transaction).filter_by(account_id=account_id).all()
+        return transactions
         pass
 
 
