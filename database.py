e2391bcccc55fc820adddba9e428bfe8d0704f6f
danialthecoder <danialtakallou@gmail.com>
2025-12-25 20:01:30 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,8 @@
 '''
+
 Connect mikone databse ro b SQLALCHEMY 
 ma fght inja tarif mikonim k b kodom database vasl beshe
 tamame code hameja hamine
-
-har projecti
-
-
 
 '''
 
@@ -13,16 +10,6 @@ har projecti
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
-#--> SQLALCHEMY --> DATABASE NISTA --> vasete beyne python database
-
-'''
-hard --> PostgreSQL , SQLite , MySQL ,....
-
-memory --> Redis 
-
-'''
-
-#mysql , ....
 DATABASE_URL = "sqlite:///database.db"
 
 engine= create_engine(DATABASE_URL,echo=False, future=True)
@@ -31,8 +18,6 @@ Base = declarative_base()
 
 SessionLocal = sessionmaker(bind=engine , autoflash=False, autocommit=False,future=True )
 
-
-#helper
 
 def get_session():
     return SessionLocal()
